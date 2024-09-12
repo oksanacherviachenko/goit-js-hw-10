1a506a169d9fe88e562d7250d01c544b4b70f483
Oksana Cherviachenko <kvintinovna@gmail.com>
2024-09-12 22:21:09 +0200

    config timer

--- src/js/1-timer.js
+++ src/js/1-timer.js
@@ -1,1 +1,10 @@
-console.log('Timer');
+
+import flatpickr from "flatpickr";
+
+import "flatpickr/dist/flatpickr.min.css";
+const flatpickr = require("flatpickr");
+
+
+import iziToast from "izitoast";
+
+import "izitoast/dist/css/iziToast.min.css";
